e4b3d9b0e8b3990d86976386a95b189570142b89
Byron Tran <159394559+byrontran@users.noreply.github.com>
2026-02-22 23:02:37 -0800

    Scaffold TCP server implementation.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,4 +1,32 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net"
+)
+
 func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close() // always close listener before process exits.
+
+	// Loop infinitely for pending connections
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// in "net" package example, connection is handled in a
+		// concurrent goroutine while the server continues listening
+		// for more acceptions. Do we want this in our implementation?
+		go func(c net.Conn) {
+			fmt.Printf("Connection Accepted!")
+			c.Close()
+		}(conn)
+
+	}
 }
--- pkg/transform/transform.go
+++ pkg/transform/transform.go
@@ -3,21 +3,21 @@ package transform
 import "fmt"
 
 func Encode(input string) string {
-  result := make([]byte, len(input))
+	result := make([]byte, len(input))
 
-  for index := range input {
-    result[index] = input[index] + 1
+	for index := range input {
+		result[index] = input[index] + 1
 
-    // TODO @byrontran: Remove
-    fmt.Printf("Char at index %d: %c\n", index, input[index])
-    fmt.Printf("Encoded char: %c\n", result[index])
-  }
-  
-  return string(result)
+		// TODO @byrontran: Remove
+		fmt.Printf("Char at index %d: %c\n", index, input[index])
+		fmt.Printf("Encoded char: %c\n", result[index])
+	}
+
+	return string(result)
 }
 
 // Optional functionality
 // TODO @byrontran: Come back later and write this?
 func Decode(intput string) string {
-  return "TODO: Implement the Decode() function in `pkg/transform/transform.go`."
+	return "TODO: Implement the Decode() function in `pkg/transform/transform.go`."
 }
