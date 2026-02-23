216cfcad4e12544d55316beff4212f7d86d62fb8
Byron Tran <159394559+byrontran@users.noreply.github.com>
2026-02-22 18:12:06 -0800

    Implement ASCII shift encoder.

--- pkg/transform/transform.go
+++ pkg/transform/transform.go
@@ -1,10 +1,23 @@
 package transform
 
+import "fmt"
+
 func Encode(input string) string {
-	return "Implement the Encode() function."
+  result := make([]byte, len(input))
+
+  for index := range input {
+    result[index] = input[index] + 1
+
+    // TODO @byrontran: Remove
+    fmt.Printf("Char at index %d: %c\n", index, input[index])
+    fmt.Printf("Encoded char: %c\n", result[index])
+  }
+  
+  return string(result)
 }
 
 // Optional functionality
+// TODO @byrontran: Come back later and write this?
 func Decode(intput string) string {
-	return "Implement the Decode() function."
+  return "TODO: Implement the Decode() function in `pkg/transform/transform.go`."
 }
