eed91445bdf1987d585ce3780a1471e541283bd3
Byron Tran <159394559+byrontran@users.noreply.github.com>
2026-02-23 15:02:32 -0800

    Implement client, server encoding and printing.

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net"
+	"time"
+)
+
 func main() {
+	// implement a TCP client as a Dialer?
+	var dialer net.Dialer
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	conn, err := dialer.DialContext(ctx, "tcp", "localhost:8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("Hello World"))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,18 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"tcp-sockets/pkg/transform"
 )
+
+// is it fine that this is out of main scope?
+const BYTE_LIMIT = 256
 
 func main() {
 	listener, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	defer listener.Close() // always close listener before process exits.
 
 	// Loop infinitely for pending connections
@@ -24,7 +29,22 @@ func main() {
 		// concurrent goroutine while the server continues listening
 		// for more acceptions. Do we want this in our implementation?
 		go func(c net.Conn) {
-			fmt.Printf("Connection Accepted!")
+			fmt.Printf("Connection Accepted!\n")
+
+			buffer := make([]byte, BYTE_LIMIT)
+			num_bytes, err := conn.Read(buffer)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if num_bytes > BYTE_LIMIT {
+				fmt.Printf("This message exceeds the server's 256 character limit.\n")
+				c.Close()
+			}
+
+			encoded_str := transform.Encode(string(buffer))
+			fmt.Println(encoded_str)
+
 			c.Close()
 		}(conn)
 
